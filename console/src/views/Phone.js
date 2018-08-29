7ca8e98e01e70f8ab193fe53130678b27c4f2a4f
Blake Stephens <blake.stephens@lge.com>
2018-08-28 17:10:51 -0700

    Phone buttons' got letters now

--- console/src/views/Phone.js
+++ console/src/views/Phone.js
@@ -3,8 +3,15 @@ import kind from '@enact/core/kind';
 import {Row, Column, Cell} from '@enact/ui/Layout';
 import React from 'react';
 
+import css from './Phone.less';
+
 const Phone = kind({
 	name: 'Phone',
+
+	styles: {
+		css,
+		className: 'phone'
+	},
 
 	render: (props) => (
 		<Column {...props} align="center">
@@ -17,28 +24,28 @@ const Phone = kind({
 					<Cell>
 						<Row align="center center">
 							<Cell shrink component={Button} type="grid">1</Cell>
-							<Cell shrink component={Button} type="grid">2</Cell>
-							<Cell shrink component={Button} type="grid">3</Cell>
+							<Cell shrink component={Button} type="grid" data-subtitle="ABC">2</Cell>
+							<Cell shrink component={Button} type="grid" data-subtitle="DEF">3</Cell>
 						</Row>
 					</Cell>
 					<Cell>
 						<Row align="center center">
-							<Cell shrink component={Button} type="grid">4</Cell>
-							<Cell shrink component={Button} type="grid">5</Cell>
-							<Cell shrink component={Button} type="grid">6</Cell>
+							<Cell shrink component={Button} type="grid" data-subtitle="GHI">4</Cell>
+							<Cell shrink component={Button} type="grid" data-subtitle="JKL">5</Cell>
+							<Cell shrink component={Button} type="grid" data-subtitle="MNO">6</Cell>
 						</Row>
 					</Cell>
 					<Cell>
 						<Row align="center center">
-							<Cell shrink component={Button} type="grid">7</Cell>
-							<Cell shrink component={Button} type="grid">8</Cell>
-							<Cell shrink component={Button} type="grid">9</Cell>
+							<Cell shrink component={Button} type="grid" data-subtitle="PQRS">7</Cell>
+							<Cell shrink component={Button} type="grid" data-subtitle="TUV">8</Cell>
+							<Cell shrink component={Button} type="grid" data-subtitle="WXYZ">9</Cell>
 						</Row>
 					</Cell>
 					<Cell>
 						<Row align="center center">
 							<Cell shrink component={Button} type="grid">*</Cell>
-							<Cell shrink component={Button} type="grid">0</Cell>
+							<Cell shrink component={Button} type="grid" data-subtitle="+">0</Cell>
 							<Cell shrink component={Button} type="grid">#</Cell>
 						</Row>
 					</Cell>
