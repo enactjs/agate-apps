1970e854d1da2005b406e3578362893afbcc9741
Dave Freeman <dave.freeman@lge.com>
2018-08-30 13:55:26 -0500

    TabbedPanels first look

--- console/src/App/App.js
+++ console/src/App/App.js
@@ -1,12 +1,15 @@
-import kind from '@enact/core/kind';
 import AgateDecorator from '@enact/agate/AgateDecorator';
+import kind from '@enact/core/kind';
 import React from 'react';
+import {TabbedPanels} from '@enact/agate/Panels';
 
 import MainPanel from '../views/MainPanel';
 
 import css from './App.less';
 
-const App = kind({
+const divTest = (props) => <div {...props} />;
+
+const AppBase = kind({
 	name: 'App',
 
 	styles: {
@@ -15,10 +18,44 @@ const App = kind({
 	},
 
 	render: (props) => (
-		<div {...props}>
-			<MainPanel />
-		</div>
+		<TabbedPanels
+			{...props}
+			tabOrientation="vertical"
+			tabs={[
+				{title: 'Main Panel', view: MainPanel},
+				{title: 'Tab 1', view: divTest, viewProps: {
+					children: 'View 1'
+				}},
+				{title: 'Tab 2', view: divTest, viewProps: {
+					children: 'View 2'
+				}}
+			]}
+		/>
 	)
 });
+
+class App extends React.Component {
+	constructor (props) {
+		super(props);
+		this.state = {
+			index: props.index || 0
+		};
+	}
+	onSelect = (ev) => {
+		const {selected: index} = ev;
+		if (typeof index === 'number' && index !== this.state.index) {
+			this.setState({index});
+		}
+	};
+	render () {
+		const props = Object.assign({}, this.props);
+		props.index = this.state.index;
+		props.onSelect = this.onSelect;
+
+		return(
+			<AppBase {...props} />
+		);
+	}
+}
 
 export default AgateDecorator(App);
