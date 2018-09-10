b5fca5dd67299e320a3720b9f1edd1b5c4abf8f3
Hailey Ryu <hangyeol.ryu@lge.com>
2018-09-10 13:17:39 -0700

    add feature demo mode back.

--- console/src/views/Phone.js
+++ console/src/views/Phone.js
@@ -2,16 +2,18 @@
 import Button from '@enact/agate/Button';
 import Input from '@enact/agate/Input';
 import Changeable from '@enact/ui/Changeable';
+import Toggleable from '@enact/ui/Toggleable';
 import Scroller from '@enact/ui/Scroller';
 import Icon from '@enact/agate/Icon';
 import {Column, Cell} from '@enact/ui/Layout';
-import {adaptEvent, forward, handle} from '@enact/core/handle';
+import {adaptEvent, forKey, forward, handle, oneOf} from '@enact/core/handle';
 import kind from '@enact/core/kind';
 import {Panel} from '@enact/agate/Panels';
 import PropTypes from 'prop-types';
 import React from 'react';
 
 import Dialer from '../components/Dialer';
+import CallPopup from '../components/CallPopup';
 import ContactThumbnail from '../components/ContactThumbnail';
 import css from './Phone.less';
 
@@ -38,6 +40,19 @@ const contacts = [
 	},
 ]
 
+const forwardClear = adaptEvent(
+	(ev, {value}) => ({value: value ? value.substring(0, value.length - 1) : ''}),
+	forward('onChange')
+);
+
+const isFromDecorator = ({target}) => target.nodeName !== 'INPUT';
+
+const isDigit = ({keyCode}) => keyCode >= 48 && keyCode <= 57;
+const appendValue = appender => adaptEvent(
+	(ev, {value}) => ({value: `${value}${appender(ev)}`}),
+	forward('onChange')
+);
+
 const PhoneBase = kind({
 	name: 'Phone',
 	styles: {
@@ -47,6 +62,8 @@ const PhoneBase = kind({
 
 	propTypes: {
 		onChange: PropTypes.func,
+		onTogglePopup: PropTypes.func,
+		showPopup: PropTypes.bool,
 		value: PropTypes.string
 	},
 
@@ -55,16 +72,11 @@ const PhoneBase = kind({
 	},
 
 	handlers: {
-		onClear: handle(
-			adaptEvent(
-				(ev, {value}) => ({value: value ? value.substring(0, value.length - 1) : ''}),
-				forward('onChange')
-			)
-		),
-		onSelectDigit: handle(
-			adaptEvent(
-				({value: digit}, {value}) => ({value: `${value}${digit}`}),
-				forward('onChange')
+		handleInputKeyDown: handle(
+			isFromDecorator,
+			oneOf(
+				[forKey('backspace'), forwardClear],
+				[isDigit, appendValue(ev => ev.keyCode - 48)]
 			)
 		),
 		onContactClick: handle(
@@ -73,10 +85,12 @@ const PhoneBase = kind({
 				forward('onChange')
 			)
 		),
+		onClear: handle(forwardClear),
+		onSelectDigit: handle(appendValue(ev => ev.value))
 	},
 
 
-	render: ({onChange, onClear, onSelectDigit, onContactClick, value, ...rest}) => {
+	render: ({handleInputKeyDown, onContactClick, onChange, onClear, onSelectDigit, onTogglePopup, showPopup, value, ...rest}) => {
 		const	contactList = contacts.map(contact => {
 			return (
 				<ContactThumbnail
@@ -90,17 +104,26 @@ const PhoneBase = kind({
 					<Cell shrink className="number-field">
 						<Icon>user</Icon>
 						<Input
+							dismissOnEnter
+							onKeyDown={handleInputKeyDown}
 							onChange={onChange}
 							placeholder="Phone Number ..."
 							value={value}
 						/>
-						<Icon onClick={onClear} css={css} >{"\u232B"}</Icon>
+						<Icon onClick={onClear} css={css} >\u232B</Icon>
 					</Cell>
-					<Cell className={css.dialer}>
+					<Cell className="dialer-grid">
 						<Dialer align="center center" onSelectDigit={onSelectDigit} />
 					</Cell>
 					<Cell shrink className="call">
-						<Button type="grid" highlighted style={{width: '300px'}}>Call</Button>
+						<Button
+							disabled={!value}
+							onClick={onTogglePopup}
+							type="grid"
+							highlighted
+							style={{width: '300px'}}
+							>Call
+						</Button>
 					</Cell>
 					<Cell
 						component={Scroller}
@@ -111,11 +134,22 @@ const PhoneBase = kind({
 						{contactList}
 					</Cell>
 				</Column>
+				<CallPopup
+					contactName=""
+					onCallEnd={onTogglePopup}
+					open={showPopup}
+					phoneNumber={value}
+				/>
 			</Panel>
 		)
 	}
 });
 
-const Phone = Changeable(PhoneBase);
+const Phone = Toggleable(
+	{prop: 'showPopup', toggle: 'onTogglePopup'},
+	Changeable(
+		PhoneBase
+	)
+);
 
 export default Phone;
