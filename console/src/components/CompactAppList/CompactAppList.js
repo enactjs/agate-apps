e83f6a0509618312b379b251073038cf91ad1ab5
Hailey Ryu <hangyeol.ryu@lge.com>
2018-10-04 15:27:51 -0700

    update

--- console/src/components/CompactAppList/CompactAppList.js
+++ console/src/components/CompactAppList/CompactAppList.js
@@ -1,13 +1,14 @@
 import kind from '@enact/core/kind';
 import {Row} from '@enact/ui/Layout';
 import React from 'react';
+import PropTypes from 'prop-types';
 import AppIconCell from '../AppIconCell';
 
 const CompactAppList = kind({
 	name: 'CompactAppList',
 
-	styles: {
-		className: 'compact'
+	propTypes: {
+		onSelect: PropTypes.func
 	},
 
 	handlers: {
@@ -18,17 +19,17 @@ const CompactAppList = kind({
 		}
 	},
 
-	render: ({onTabChange, ...rest}) => (
-		<div>
+	render: ({onTabChange}) => (
+		<React.Fragment>
 			<Row align="start center">
-        <AppIconCell size="40%" icon="compass">Navigation</AppIconCell>
-        <AppIconCell size="40%" icon="audio">Audio</AppIconCell>
-      </Row>
-      <Row align="start center">
-        <AppIconCell size="40%" icon="resumeplay">Multimedia</AppIconCell>
-        <AppIconCell size="40%" icon="gear" data-tabindex={4} onKeyUp={onTabChange} onClick={onTabChange}>Settings</AppIconCell>
-      </Row>
-    </div>
+				<AppIconCell size="40%" icon="compass">Navigation</AppIconCell>
+				<AppIconCell size="40%" icon="audio">Audio</AppIconCell>
+			</Row>
+			<Row align="start center">
+				<AppIconCell size="40%" icon="resumeplay">Multimedia</AppIconCell>
+				<AppIconCell size="40%" icon="gear" data-tabindex={4} onKeyUp={onTabChange} onClick={onTabChange}>Settings</AppIconCell>
+			</Row>
+		</React.Fragment>
 	)
 });
 
