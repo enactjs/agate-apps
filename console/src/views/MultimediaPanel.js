bfa8b27e69e26bff56f391d54c5609bd62ce3906
Dave Freeman <dave.freeman@lge.com>
2018-10-22 15:46:38 -0500

    ensure unique key for items in lists

--- console/src/views/MultimediaPanel.js
+++ console/src/views/MultimediaPanel.js
@@ -27,10 +27,29 @@ const Multimedia = kind({
 	render: ({css, onVideoSelect, ...rest}) => (
 		<Panel {...rest}>
 			<Row className="enact-fit" align=" center">
+<<<<<<< HEAD
 				<Cell size="40%">
 					<Item onClick={onVideoSelect({title: 'Pasta day', url: 'https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fjtbcnews%2Fvideos%2F2092788370971685%2F&show_text=0&width=560?autoplay=1'})}>{'https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fjtbcnews%2Fvideos%2F2092788370971685%2F&show_text=0&width=560'}</Item>
 					<Item onClick={onVideoSelect({title: 'Jssie J - Not my ex', url: 'https://www.youtube.com/embed/FvcNr_5uWgY?autoplay=1'})}>{'https://www.youtube.com/embed/RYdQwLP1gfU'}</Item>
 					<Item onClick={onVideoSelect({title: 'lofi music live', url: 'https://www.youtube.com/embed/LsBrT6vbQa8?autoplay=1'})}>{'https://www.youtube.com/embed/LsBrT6vbQa8'}</Item>
+=======
+				<Cell>
+					{
+						this.videos.map((video, index) => {
+							return <Item
+								key={index}
+								onClick={this.onVideoSelect({
+									type: 'youtube',
+									title: video.snippet.title,
+									url: `https://www.youtube.com/embed/${video.id}?autoplay=1`
+								})}
+							>
+								<img src={video.snippet.thumbnails.default.url} />
+								{video.snippet.title}
+							</Item>;
+						})
+					}
+>>>>>>> ensure unique key for items in lists
 				</Cell>
 			</Row>
 		</Panel>
