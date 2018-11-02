32f566ec4dca969f1e87e17c8d4743d4e7f6114f
Dave Freeman <dave.freeman@lge.com>
2018-11-02 13:04:02 -0500

    add some state to HVAC view

--- console/src/views/HVAC.js
+++ console/src/views/HVAC.js
@@ -1,18 +1,17 @@
 import Divider from '@enact/agate/Divider';
+import hoc from '@enact/core/hoc';
+import kind from '@enact/core/kind';
+import Layout, {Cell, Row} from '@enact/ui/Layout';
 import {Panel} from '@enact/agate/Panels';
 import Picker from '@enact/agate/Picker';
+import React from 'react';
 import {ResponsiveBox} from '@enact/agate/DropManager';
 import SliderButton from '@enact/agate/SliderButton';
 import ToggleButton from '@enact/agate/ToggleButton';
-import kind from '@enact/core/kind';
-import Layout, {Cell, Row} from '@enact/ui/Layout';
-import React from 'react';
 
 import CustomLayout, {SaveLayoutArrangement} from '../components/CustomLayout';
 
 import css from './HVAC.less';
-
-const temps = ['HI', '74°', '73°', '72°', '71°', '70°', '69°', '68°', '67°', '66°', 'LO'];
 
 const ResponsiveLayout = ResponsiveBox(({containerShape, ...rest}) => {
 	const orientation = (containerShape.orientation === 'portrait') ? 'vertical' : 'horizontal';
@@ -33,33 +32,82 @@ const HvacBase = kind({
 		className: 'hvac'
 	},
 
-	render: ({arrangeable, arrangement, onArrange, ...rest}) => (
+	render: ({acSelected, arrangeable, arrangement, autoSelected, onArrange, onToggleAc, onToggleAuto, speeds, temps, ...rest}) => (
 		<Panel {...rest}>
 			<CustomLayout arrangeable={arrangeable} arrangement={arrangement} onArrange={onArrange}>
 				<top>
 					<Divider>
 						Fan Speed
 					</Divider>
-					<SliderButton>
-						{['Off', 'Low', 'Medium', 'High']}
+					<SliderButton disabled={autoSelected}>
+						{speeds}
 					</SliderButton>
 				</top>
 				<Row className={css.above} align="center space-around">
-					<Cell component={ToggleButton} shrink icon="heatseatleft" type="grid" className={css.button} underline />
-					<Cell component={ToggleButton} shrink type="grid" className={css.button}>A/C</Cell>
-					<Cell component={ToggleButton} shrink icon="heatseatright" type="grid" className={css.button} underline />
+					<Cell
+						className={css.button}
+						component={ToggleButton}
+						icon="heatseatleft"
+						shrink
+						type="grid"
+						underline
+					/>
+					<Cell
+						className={css.button}
+						component={ToggleButton}
+						onClick={onToggleAc}
+						selected={acSelected}
+						shrink
+						type="grid"
+						underline
+					>
+						A/C
+					</Cell>
+					<Cell
+						className={css.button}
+						component={ToggleButton}
+						icon="heatseatright"
+						shrink
+						type="grid"
+						underline
+					/>
 				</Row>
 				<Row className={css.below} align="center space-around">
-					<Picker orientation="vertical" className={css.picker}>
+					<Cell
+						className={css.picker}
+						component={Picker}
+						orientation="vertical"
+						shrink
+					>
 						{temps}
-					</Picker>
-					<div className={css.stackedButtons}>
-						<ToggleButton type="grid" className={css.button}>AUTO</ToggleButton>
-						<ToggleButton type="grid" icon="aircirculation" className={css.button} />
-					</div>
-					<Picker orientation="vertical" className={css.picker}>
+					</Cell>
+					<Cell
+						className={css.stackedButtons}
+						shrink
+					>
+						<ToggleButton
+							type="grid"
+							className={css.button}
+							onClick={onToggleAuto}
+							selected={autoSelected}
+							underline
+						>
+							AUTO
+						</ToggleButton>
+						<ToggleButton
+							className={css.button}
+							icon="aircirculation"
+							type="grid"
+						/>
+					</Cell>
+					<Cell
+						className={css.picker}
+						component={Picker}
+						orientation="vertical"
+						shrink
+					>
 						{temps}
-					</Picker>
+					</Cell>
 				</Row>
 				<bottom>
 					<ResponsiveLayout wrap>
@@ -75,7 +123,59 @@ const HvacBase = kind({
 	)
 });
 
-const Hvac = SaveLayoutArrangement('hvac')(HvacBase);
+const defaultConfig = {
+	acSelected: false,
+	autoSelected: false,
+	speeds: ['Off', 'Low', 'Medium', 'High'],
+	temps: ['HI', '74°', '73°', '72°', '71°', '70°', '69°', '68°', '67°', '66°', 'LO']
+};
+
+const HvacDecorator = hoc(defaultConfig, (configHoc, Wrapped) => {
+	return class extends React.Component {
+		static displayName = HvacDecorator;
+
+		constructor (props) {
+			super(props);
+
+			this.state = {
+				acSelected: configHoc.acSelected,
+				autoSelected: configHoc.autoSelected,
+				speeds: configHoc.speeds,
+				temps: configHoc.temps
+			};
+		}
+
+		toggleAc = () => {
+			this.setState(({acSelected}) => {
+				return {acSelected: !acSelected};
+			});
+		};
+
+		toggleAuto = () => {
+			this.setState(({acSelected, autoSelected}) => {
+				return {
+					acSelected: !autoSelected ? true : acSelected,
+					autoSelected: !autoSelected
+				};
+			});
+		};
+
+		render () {
+			const props = {
+				...this.state,
+				onToggleAc: this.toggleAc,
+				onToggleAuto: this.toggleAuto
+			};
+			return (
+				<Wrapped
+					{...props}
+				/>
+			);
+		}
+	}
+});
+
+const Hvac = SaveLayoutArrangement('hvac')(HvacDecorator(HvacBase));
 
 export default Hvac;
 export {
