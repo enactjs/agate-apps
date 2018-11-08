262df759889a423780056877a72cb8e734dd05be
Blake Stephens <blake.stephens@lge.com>
2018-11-07 17:02:53 -0800

    Cleanup of packages, simplify AppContext, update initial user selection
    I'd added unnecessary "fancy" to the user database functions which didn't help readability and just made things a little too complicated.
    The initial user selection screen is now updated to look like the wireframes we've discussed in meetings and drawn on the board. The interaction pattern was also simplified to allow one-touch action rather than selection and acknowledgement. We can provide a small "back" button on the eventual welcome screen that goes back to the user selection to prevent accidents.
    There were also some unnecessary buttons on the Dashboard view
    Updated the spacing/positioning on the CompactRadio component for a little nicer layout

--- console/src/components/WelcomePopup/WelcomePopup.js
+++ console/src/components/WelcomePopup/WelcomePopup.js
@@ -1,9 +1,10 @@
-import Button from '@enact/agate/Button';
 import Divider from '@enact/agate/Divider';
 import FullscreenPopup from '@enact/agate/FullscreenPopup';
 import {Panel, Panels} from '@enact/agate/Panels';
-import SwitchItem from '@enact/agate/SwitchItem';
+import LabeledIconButton from '@enact/agate/LabeledIconButton';
+import {handle, forward} from '@enact/core/handle';
 import kind from '@enact/core/kind';
+import {Column, Row, Cell} from '@enact/ui/Layout';
 import Group from '@enact/ui/Group';
 import PropTypes from 'prop-types';
 import React from 'react';
@@ -19,30 +20,49 @@ const WelcomePopupBase = kind({
 		userId: PropTypes.number
 	},
 
-	render: ({onClose, updateUser, userId, ...rest}) => (
-		<FullscreenPopup {...rest} onClose={onClose}>
-			<Panels>
-				<Panel>
-					<Divider startSection>User Selection</Divider>
-					<Group
-						childComponent={SwitchItem}
-						defaultSelected={userId - 1}
-						onSelect={updateUser}
-						select="radio"
-						selectedProp="selected"
+	handlers: {
+		selectUserAndContinue: handle(
+			forward('updateUser'),
+			// After the personalized welcome screen is added, that transition will be added here
+			// and `onClose` will be applied to the "GO" button (to be added later) and will be
+			// removed from here.
+			forward('onClose')
+		)
+	},
 
-					>
-						{['User 1', 'User 2', 'User 3']}
-					</Group>
-					<Button onClick={onClose}>Select</Button>
-				</Panel>
-			</Panels>
-		</FullscreenPopup>
-	)
+	render: ({selectUserAndContinue, ...rest}) => {
+		delete rest.onClose;
+		delete rest.updateUser;
+		// delete rest.userId;
+		return (
+			<FullscreenPopup {...rest}>
+				<Panels>
+					<Panel>
+						<Column align="stretch center">
+							<Cell component={Divider} startSection shrink>User Selection</Cell>
+							<Cell shrink>
+								<Row
+									component={Group}
+									childComponent={Cell}
+									itemProps={{component: LabeledIconButton, shrink: true, icon: 'user'}}
+									onSelect={selectUserAndContinue}
+									select="radio"
+									selectedProp="selected"
+									wrap
+									align="start space-evenly"
+								>
+									{['User 1', 'User 2', 'User 3']}
+								</Row>
+							</Cell>
+						</Column>
+					</Panel>
+				</Panels>
+			</FullscreenPopup>
+		);
+	}
 });
 
-const WelcomePopup = AppContextConnect(({userId, updateAppState}) => ({
-	userId: userId,
+const WelcomePopup = AppContextConnect(({updateAppState}) => ({
 	updateUser: ({selected}) => {
 		updateAppState((state) => {
 			state.userId = selected + 1;
