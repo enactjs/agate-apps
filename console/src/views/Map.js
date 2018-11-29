b82a156ef6e77f1f24500fa6f9a957f6156bfdff
Dave Freeman <dave.freeman@lge.com>
2018-11-29 16:55:56 -0600

    give Map some brains

--- console/src/views/Map.js
+++ console/src/views/Map.js
@@ -1,26 +1,82 @@
+import Button from '@enact/agate/Button';
+import hoc from '@enact/core/hoc';
 import kind from '@enact/core/kind';
 import {Panel} from '@enact/agate/Panels';
 import React from 'react';
 
 import MapCore from '../components/MapCore';
 
-// import css from './Map.less';
-
-const MapViewBase = kind({
-	name: 'MapView',
+const MapBase = kind({
+	name: 'Map',
 
 	// styles: {
 	// 	css,
 	// 	className: 'map'
 	// },
 
-	render: ({...rest}) => {
+	render: ({changePosition, onSetDestination, ...rest}) => {
 		return (
 			<Panel {...rest}>
-				<MapCore />
+				<MapCore>
+					<tools>
+						<Button alt="POI search" icon="search" />
+						<Button alt="Propose new destination" icon="arrowhookleft" onClick={changePosition} />
+						<Button alt="Navigate here" icon="play" onClick={onSetDestination} />
+					</tools>
+				</MapCore>
 			</Panel>
 		);
 	}
 });
 
-export default MapViewBase;
+const MapBrains = hoc((configHoc, Wrapped) => {
+	const positions = [
+		{lat: 37.788818, lon: -122.404568}, // LG office
+		{lat: 37.791356, lon: -122.400823}, // Blue Bottle Coffee
+		{lat: 37.788988, lon: -122.401076},
+		{lat: 37.7908574786, lon: -122.399391029},
+		{lat: 37.786116, lon: -122.402140}
+	];
+
+	return class extends React.Component {
+		static displayName = 'CompactMapBrains';
+		constructor (props) {
+			super(props);
+			this.state = {
+				positionIndex: 0,
+				destination: null
+			};
+		}
+
+		handleSetDestination = () => {
+			// Take our current position and assign it as our internal (local to this HOC) destination
+			this.setState(({positionIndex}) => ({destination: [positions[positionIndex]]}));
+		}
+
+		changePosition = () => {
+			console.log('CHANGE POSITION');
+			this.setState(({positionIndex}) => ({
+				// go to the next position in the list
+				positionIndex: ((positionIndex + 1) % positions.length)
+			}));
+		}
+
+		render () {
+			return (
+				<Wrapped
+					{...this.props}
+					changePosition={this.changePosition}
+					follow={this.state.follow}
+					onSetDestination={this.handleSetDestination}
+					destination={this.state.destination}
+					proposedDestination={[positions[this.state.positionIndex]]}
+				/>
+			);
+		}
+	};
+});
+
+const MapView = MapBrains(MapBase);
+
+export default MapView;
+export {MapView as Map};
