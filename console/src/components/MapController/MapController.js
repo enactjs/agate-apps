47132282bc00711f584a23007863b43e517cd117
Stephen Choi <stephen.choi@lge.com>
2018-12-26 14:09:47 -0800

    PLAT-71626: Fix navigation button alignment in MapController
    Enact-DCO-1.0-Signed-off-by: Stephen Choi <stephen.choi@lge.com>

--- console/src/components/MapController/MapController.js
+++ console/src/components/MapController/MapController.js
@@ -232,16 +232,17 @@ const MapControllerHoc = hoc((configHoc, Wrapped) => {
 							{
 								!noStartStopToggle && destination &&
 								<Cell shrink className={css.columnCell}>
-									<ToggleButton
-										className={css.button}
-										small
-										// We want to be able to factor in the autonomous state, but
-										// perhaps that needs to happen in ServiceLayer, and not here.
-										selected={destination && navigating}
-										onToggle={this.startNavigation}
-										toggleOnLabel="Stop Navigation"
-										toggleOffLabel="Start Navigation"
-									/>
+									<Column>
+										<ToggleButton
+											small
+											// We want to be able to factor in the autonomous state, but
+											// perhaps that needs to happen in ServiceLayer, and not here.
+											selected={destination && navigating}
+											onToggle={this.startNavigation}
+											toggleOnLabel="Stop Navigation"
+											toggleOffLabel="Start Navigation"
+										/>
+									</Column>
 								</Cell>
 							}
 						</Column>
