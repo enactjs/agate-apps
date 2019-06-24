5838b2f4dbc165b4e112e7beb1f1f41382e8e6f8
Blake Stephens <blake.stephens@lge.com>
2019-06-24 12:21:47 -0700

    Added the ability to specify a different skin just by changing the URL
    `?skin=carbon`, etc
    
    Enact-DCO-1.0-Signed-off-by: Blake Stephens <blake.stephens@lge.com>

--- kitchen-sink/src/index.js
+++ kitchen-sink/src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import {render} from 'react-dom';
+import qs from 'query-string';
+
 import App from './App';
 
-const appElement = (
-	<App
-		skin="cobalt"
-		accent="#FC7982"
-		highlight="#FFFFFF"
-	/>
-);
+let appElement = <App />;
 
 // In a browser environment, render instead of exporting
 if (typeof window !== 'undefined') {
+	const args = qs.parse(window.location.search);
+	const skin = args.skin || 'cobalt';
+
+	appElement = (
+		<App
+			skin={skin}
+			accent="#FC7982"
+			highlight="#FFFFFF"
+		/>
+	);
+
 	render(appElement, document.getElementById('root'));
 }
 
