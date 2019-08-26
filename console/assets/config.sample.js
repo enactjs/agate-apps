c6e2d21619ef7286ae2c4b46668529cb4d4e8a92
Baekwoo Jung <baekwoo.jung@lge.com>
2019-08-26 15:17:52 +0900

    Add sample file of config.js
    Enact-DCO-1.0-Signed-off-by: Baekwoo Jung (baekwoo.jung@lge.com)

---
+++ console/assets/config.sample.js
@@ -0,0 +1,35 @@
+const
+    communicationServerHost = '127.0.0.1:3303',
+    servicesLayerHost = '127.0.0.1:9090';
+
+const
+    mapApiKey = '',
+    weatherApiKey = '';
+
+const
+    rssiLaura = -75,
+    rssiThomas = -75;
+
+function getCommunicationServerHost () {
+    return communicationServerHost;
+}
+
+function getServicesLayerHost () {
+    return servicesLayerHost;
+}
+
+function getMapApiKey () {
+    return mapApiKey;
+}
+
+function getWeatherApiKey () {
+    return weatherApiKey;
+}
+
+function getRssiLaura () {
+    return rssiLaura;
+}
+
+function getRssiThomas () {
+    return rssiThomas;
+}
