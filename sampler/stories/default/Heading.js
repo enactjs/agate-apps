fe4d4198bb517ece99248b60bc8236112c99a715
Ryan Duffy <ryan.duffy@lge.com>
2019-09-04 16:32:31 -0700

    PLAT-84726: Add Heading sample
    Signed-off-by: Ryan Duffy <ryan.duffy@lge.com>

---
+++ sampler/stories/default/Heading.js
@@ -0,0 +1,32 @@
+import Heading, {HeadingBase} from '@enact/agate/Heading';
+
+import React from 'react';
+import {storiesOf} from '@storybook/react';
+
+import {boolean, select, text} from '../../src/enact-knobs';
+import {mergeComponentMetadata} from '../../src/utils';
+
+Heading.displayName = 'Heading';
+const Config = mergeComponentMetadata('Heading', Heading, HeadingBase);
+
+const prop = {
+	colors: ['', '#E6444B', '#FDC902', '#986AAD', '#4E75E1', '#30CC83', '#44C8D5', '#47439B', '#2D32A6', '#4E75E1'],
+	sizes: ['', 'small', 'medium', 'large', 'title', 'none']
+};
+
+storiesOf('Agate', module)
+	.add(
+		'Heading',
+		() => (
+			<Heading
+				customColor={select('customColor', prop.colors, Config)}
+				showLine={boolean('showLine', Config)}
+				size={select('size', prop.sizes, Config)}
+			>
+				{text('children', Heading, 'Heading Text')}
+			</Heading>
+		),
+		{
+			text: 'The basic Heading'
+		}
+	);
