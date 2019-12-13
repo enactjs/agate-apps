74a91aa88c5daa23630e52a3ca962b44f5428237
Teck Liew <teck.liew@lge.com>
2019-12-12 17:57:10 -0800

    add CheckboxItem sample

---
+++ sampler/stories/default/CheckboxItem.js
@@ -0,0 +1,29 @@
+import CheckboxItem from '@enact/agate/CheckboxItem';
+import React from 'react';
+import {storiesOf} from '@storybook/react';
+import {action} from '@storybook/addon-actions';
+
+import {boolean, text} from '../../src/enact-knobs';
+import {mergeComponentMetadata} from '../../src/utils';
+
+CheckboxItem.displayName = 'CheckboxItem';
+const Config = mergeComponentMetadata('CheckboxItem', CheckboxItem);
+
+storiesOf('Agate', module)
+	.add(
+		'CheckboxItem',
+		() => (
+			<CheckboxItem
+				disabled={boolean('disabled', Config)}
+				inline={boolean('inline', Config)}
+				onToggle={action('onToggle')}
+			>
+				{text('children', Config, 'Hello CheckboxItem')}
+			</CheckboxItem>
+		),
+		{
+			info: {
+				text: 'Basic usage of CheckboxItem'
+			}
+		}
+	);
