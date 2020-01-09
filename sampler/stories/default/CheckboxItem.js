50be9d08093a5e81b171e158955740704f4234c4
Ryan Duffy <ryan.duffy@lge.com>
2020-01-09 09:34:50 -0800

    remove iconPosition

--- sampler/stories/default/CheckboxItem.js
+++ sampler/stories/default/CheckboxItem.js
@@ -15,7 +15,6 @@ storiesOf('Agate', module)
 		() => (
 			<CheckboxItem
 				disabled={boolean('disabled', Config)}
-				iconPosition={select('iconPosition', ['', 'after', 'before'], Config, '')}
 				inline={boolean('inline', Config)}
 				onToggle={action('onToggle')}
 			>
