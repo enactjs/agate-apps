51ca3355c5af85414a160e0a51e48d8a340f0fae
Teck Liew <teck.liew@lge.com>
2020-01-15 16:04:30 -0800

    add knob to dynamically add children and direction

--- sampler/stories/default/Dropdown.js
+++ sampler/stories/default/Dropdown.js
@@ -3,31 +3,31 @@ import React from 'react';
 import {storiesOf} from '@storybook/react';
 import {action} from '@storybook/addon-actions';
 
-import {boolean} from '../../src/enact-knobs';
-import Item from '@enact/agate/Item';
+import {boolean, number, select} from '../../src/enact-knobs';
 import {mergeComponentMetadata} from '../../src/utils';
 
 const Config = mergeComponentMetadata('Dropdown', Dropdown, DropdownBase);
 Dropdown.displayName = 'Dropdown';
 
-const prop = {
-	colors: ['green', 'yellow', 'orange', 'red', 'black', 'gray', 'white', 'maroon', 'brown']
-};
-
 storiesOf('Agate', module)
 	.add(
 		'Dropdown',
-		() => (
-			<div>
-				<Dropdown
-					disabled={boolean('disabled', Config)}
-					onChange={action('onSelect')}
-				>
-					{prop.colors}
-				</Dropdown>
-				<Item>Default Item</Item>
-			</div>
-		),
+		() => {
+			const itemCount = number('items', Config, {range: true, min: 0, max: 50}, 5);
+			const items = (new Array(itemCount)).fill().map((i, index) => `Option ${index + 1}`);
+
+			return (
+				<div>
+					<Dropdown
+						direction={select('direction', ['up', 'down'], Config)}
+						disabled={boolean('disabled', Config)}
+						onChange={action('onSelect')}
+					>
+						{items}
+					</Dropdown>
+				</div>
+			);
+		},
 		{
 			text: 'The basic Dropdown'
 		}
