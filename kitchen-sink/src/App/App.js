c685e46587b774e310b863fa71b057e0bc462ea0
Alex Morariu <alexandru.morariu@lgepartner.com>
2021-06-02 16:38:52 +0300

    Fix

--- kitchen-sink/src/App/App.js
+++ kitchen-sink/src/App/App.js
@@ -1,6 +1,6 @@
 import kind from '@enact/core/kind';
-import ThemeDecorator from '@enact/agate/ThemeDecorator';
 import Panels from '@enact/agate/Panels';
+import ThemeDecorator from '@enact/agate/ThemeDecorator';
 
 import MainPanel from '../views/MainPanel';
 
