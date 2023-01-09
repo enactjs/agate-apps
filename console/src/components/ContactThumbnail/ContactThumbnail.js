46e245460eba96a830bf3283e2797da70661acf1
Daniel Stoian <daniel.stoian@lgepartner.com>
2023-01-09 18:50:44 +0200

    code review fix

--- console/src/components/ContactThumbnail/ContactThumbnail.js
+++ console/src/components/ContactThumbnail/ContactThumbnail.js
@@ -30,7 +30,7 @@ const ContactThumbnail = kind({
 	},
 
 	render: ({contact, onSelect, ...props}) => (
-		<div {...props} css={css} onClick={onSelect}>
+		<div {...props} onClick={onSelect}>
 			<Item label={contact.number}>
 				<Icon slot="slotBefore">user</Icon>
 				{contact.name}
