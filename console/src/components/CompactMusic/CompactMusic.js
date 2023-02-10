d7b14012478782c21afba9d6240b2b28fc7ca18d
Andrei Tirla <andrei.tirla@lgepartner.com>
2023-02-10 10:41:20 +0200

    replaced ImageItem with MediaPlayer in CompactMusic.js

--- console/src/components/CompactMusic/CompactMusic.js
+++ console/src/components/CompactMusic/CompactMusic.js
@@ -1,9 +1,10 @@
 // import Button from '@enact/agate/Button';
-import ImageItem from '@enact/agate/ImageItem';
+// import ImageItem from '@enact/agate/ImageItem';
+import MediaPlayer from '@enact/agate/MediaPlayer'
 import kind from '@enact/core/kind';
 import {Cell} from '@enact/ui/Layout';
 
-import albumArt from '../../../assets/music-album-art.jpg';
+// import albumArt from '../../../assets/music-album-art.jpg';
 import Widget from '../Widget';
 
 import css from './CompactMusic.module.less';
@@ -42,17 +43,28 @@ const CompactMusicBase = kind({
 	},
 
 	render: (props) => {
+		const audioFiles = [
+			'https://sampleswap.org/mp3/artist/254731/BossPlayer_Your-Right-Here-160.mp3',
+			'https://sampleswap.org/mp3/artist/78152/HiatusManJBanner_Show-Stopper-160.mp3',
+			'https://sampleswap.org/mp3/artist/47067/DJ-Masque_Oceanic-Dawn-160.mp3',
+			'https://sampleswap.org/mp3/artist/26546/benzoul_lovevoodoo-160.mp3',
+			'https://sampleswap.org/mp3/artist/19139/MarkNine_In-my-Place-160.mp3',
+			'https://sampleswap.org/mp3/artist/47067/DJ-Masque_Dont-Forget-To-Be-Yourself-160.mp3'
+		];
+
+		const source = audioFiles.map((audioFile, index) => (<source key={index} src={audioFile} type="audio/mp3" />));
+
 		return (
 			<Widget {...props} icon="music" title="Listen" description="Listen to your favorite tunes" view="radio" align="center center">
 				<Cell shrink>
-					<ImageItem
-						caption="The Title"
-						css={css}
-						className={css.album}
-						selectionOverlayShowing
-						src={albumArt}
-						subCaption="The Album"
-					/>
+					<MediaPlayer
+						// caption="The Title"
+						// className={css.album}
+						// css={css}
+						// selectionOverlayShowing
+						type="tiny"
+						// subCaption="The Album"
+					>{source}</MediaPlayer>
 				</Cell>
 			</Widget>
 		);
